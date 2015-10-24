f34a68cdb16eccfb65da5d394adc972961aa1ca2
Yue Gan <gany.fml@gmail.com>
2015-10-24 17:10:05 -0500

    Debug UI improved, login credentials added
    - course to user, course to homework Debug UI now show ids and names
    - Student can use both loginname and email to login
    - when regisiter, user need to prove loginname

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(user_email: params[:session][:email].downcase)
+    user = User.get_user(params[:session][:login_credential].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
       redirect_to :controller => 'users', :action => 'show_student'
