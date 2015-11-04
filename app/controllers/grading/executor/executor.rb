6aa378e8bf50b4ce848d297e227df11e95e07f26
jhfjhfj1 <jhfjhfj1@gmail.com>
2015-11-03 18:08:18 -0600

    Program execution module added.

---
+++ app/controllers/grading/executor/executor.rb
@@ -0,0 +1,10 @@
+class Executor
+  def initialize(program_path, input_path, output_path)
+    @program_path = program_path
+    @input_path = input_path
+    @output_path = output_path
+  end
+  
+  def execute
+  end
+end
---
+++ app/controllers/grading/executor/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd $1
+java Main < $2 > $3
